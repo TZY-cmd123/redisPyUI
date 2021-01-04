20f69ad43c06a1516c4cef55606bae9422cebc1e
TZY <1137721268@qq.com>
2021-01-04 21:03:39 +0800

    冲冲冲

--- MyRedis.py
+++ MyRedis.py
@@ -7,15 +7,46 @@ class RedisOp(object):
         self.host = host
         self.port = port
         self.db = db
-        self.redis_pool = redis.ConnectionPool(host=host, port=port, db=db, decode_responses=True)
+        self.redis_pool = redis.ConnectionPool(host=host, port=int(port), db=int(db), decode_responses=True) # 这里使用线程池，方便后期维护
+    def change_db(self):
+        pass # 更换数据库，目前还不知道咋操作
+
+    def get_all_key(self):
+        self.r = redis.Redis(connection_pool=self.redis_pool)
+        return self.r.keys()
 
 
 class OperationUI(object):  # 登录成功进入操作界面
     def __init__(self, host, port, db):
-        self.host = host
-        self.port = port
-        self.db = db
         self.__redis_operation = RedisOp(host=host, port=port, db=db)  # 天然的单例模式
+        self.operation_layout = [
+            [
+                sg.Text("Host:" + host + "    Port:" + port + "    Current DB:"),
+                sg.InputCombo(values=([i for i in range(17)]), default_value=(db,), key='-SelectDB-', size=(15, 1))
+            ],
+            [
+                sg.Listbox(values=(self.__redis_operation.get_all_key()), key='-SelectKey-', default_values=(0,), size=(48, 15))
+            ],
+            [sg.Button("Add", size=(15, 1)), sg.Button("Delete", size=(15, 1)),
+             sg.Button("Cancel", size=(15, 1))
+             ]]
+        self.window = sg.Window('MyRedis', self.operation_layout)
+        self.operation()
+
+    def operation(self):
+        while True:
+            event, values = self.window.read()
+            if event == sg.WIN_CLOSED or event == 'Cancel':
+                return
+            if event == 'Add':
+                sg.popup('V1.0\nThank you for using our product')  # 弹出的内容
+                # 以上是从图形界面中读取的信息
+            if event == 'Delete':
+                host = values['-Host-']
+                port = values['-Port-']
+                db = values['-DB-']
+                break
+        self.window.close()
 
 
 class LoginUI(object):
@@ -27,7 +58,8 @@ class LoginUI(object):
                              [sg.Text("Input your Port", size=(14, 1)),
                               sg.InputText(default_text='6379', key='-Port-')],
                              [sg.Text("Select your DB number", size=(18, 1)),
-                              sg.Listbox(values=([i for i in range(17)]), key='-DB-', size=(15, 3))],
+                              sg.Listbox(values=([i for i in range(17)]), default_values=(0,), key='-DB-',
+                                         size=(15, 3))],
                              [sg.Button("Connect", size=(15, 1)), sg.Button("Cancel", size=(15, 1)),
                               sg.Button("About", size=(15, 1))]]
         self.window = sg.Window('MyRedis', self.login_layout)
@@ -40,14 +72,13 @@ class LoginUI(object):
                 return
             if event == 'About':
                 sg.popup('V1.0\nThank you for using our product')  # 弹出的内容
-            # 以上是从图形界面中读取的信息
             if event == 'Connect':
                 host = values['-Host-']
                 port = values['-Port-']
                 db = values['-DB-']
                 break
         self.window.close()
-        OperationUI(host=host, port=port, db=db)
+        OperationUI(host=host, port=port, db=db[0])
 
 
 if __name__ == '__main__':
